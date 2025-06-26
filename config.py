a83001510dd1ac3f354af8767bc1bed692c0b4d8
anandparayil <anandparayil25@gmail.com>
2025-06-26 19:02:30 +0400

    modified:   .gitignore new file:   config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+from datetime import timedelta
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    
+    MYSQL_HOST = os.environ.get('MYSQL_HOST') 
+    MYSQL_USER = os.environ.get('MYSQL_USER') 
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD') 
+    MYSQL_DB = os.environ.get('MYSQL_DB') 
+    
+    PERMANENT_SESSION_LIFETIME = timedelta(hours=24)
+    
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') 
+    MAIL_PORT = int(os.environ.get('MAIL_PORT'))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') 
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') 
